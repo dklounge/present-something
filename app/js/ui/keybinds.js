d99a68a4cf53510f8256b6015d66ce984e4fde64
Bruno Trecenti <btrecent@thoughtworks.com>
2013-07-10 13:06:20 -0300

    fixed enter keybind

--- app/js/ui/keybinds.js
+++ app/js/ui/keybinds.js
@@ -2,7 +2,7 @@ ps.ui.Keybinds = function () {
   var self = {};
 
   function nextCapture(e) {
-    if (e.which === 39 || e.wich == 13) {
+    if (e.which === 39 || e.which == 13) {
       document.getElementById('next').click();
     }
   }
